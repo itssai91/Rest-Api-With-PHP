8e54f4ad4b604e6ea5f1a5874ff4adedae9ca93f
Saikrishna Panda <itssai91@gmail.com>
2021-08-27 18:15:23 +0530

    add data code added

--- DatabaseHandler.php
+++ DatabaseHandler.php
@@ -6,6 +6,11 @@ class DatabaseHandler
     private $conn;
     private $table_name = "student";
 
+    private $col_id    = "_id";
+    private $col_name  = "name";
+    private $col_roll  = "roll_no";
+    private $col_email = "email_id";
+
     function __construct($conn)
     {
         $this->conn = $conn;
@@ -16,6 +21,13 @@ class DatabaseHandler
         $query = "SELECT * FROM `$this->table_name` ";
         $result = $this->conn->query($query);
         return $result;
+    }
+
+    function add_data($name, $roll_no, $email_id)
+    {
+        $query = "INSERT INTO `$this->table_name`(`$this->col_name`, `$this->col_roll`, `$this->col_email`) VALUES('$name', '$roll_no', '$email_id')";
+        $this->conn->query($query);
+        return true;
     }
 
     function close()
---
+++ add-data.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+require_once('conn.php');
+require_once('DatabaseHandler.php');
+
+$db = new DatabaseHandler($conn);
+
+
+
+$result = $db->add_data($_GET['name'], $_GET['roll'], $_GET['email']);
+
+if ($result) {
+    echo json_encode(["status" => 200, "name" => $_GET['email']]);
+}
