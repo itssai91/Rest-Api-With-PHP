4ae1918e16ab3281d1b873396e4713eb1f4cf6e0
Saikrishna Panda <itssai91@gmail.com>
2021-08-28 11:24:56 +0530

    live server config

--- model/conn.php
+++ model/conn.php
@@ -1,9 +1,18 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$db = "test";
+//localhost server
+
+// $host = "localhost";
+// $user = "root";
+// $password = "";
+// $db = "test";
+
+//main server
+
+$db_host = "localhost";
+$db_user = "biologyc_main";
+$db_password = "sai@72465333";
+$db_name = "biologyc_inf";
 
 $conn = mysqli_connect($host, $user, $password, $db);
 
